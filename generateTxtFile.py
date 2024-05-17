0b9aaea76a129b61fc327ae00a54695b330a4add
David <162551705+DRSZL@users.noreply.github.com>
2024-05-17 09:03:43 +0200

    Update generateTxtFile.py
    refactoring

--- generateTxtFile.py
+++ generateTxtFile.py
@@ -12,19 +12,12 @@ def createTextFileFromModelPrediction(file_name, x_array):
     
     textFileName = f"{'prediction_'}{datetime.datetime.now().timestamp()}.txt"
 
-    with open(textFileName, 'w') as f:
+    prediction = model.predict(x_array)
 
-        #for i in range(len(x_array)):
-            #p2pkh_hex = converter.float_array_to_hex(x_array[i], 40)
-                      
-            #p2pkh_binary = converter.convert_to_binary(int(p2pkh_hex, 16), bitsP2PKH)
-            #p2pkh_f32 = converter.binArray2float32array(p2pkh_binary)
-           
-            prediction = model.predict(x_array)
-            
+    with open(textFileName, 'w') as f:      
+                       
             # generate txt file with predicted keys from public key
             for i in range(len(prediction)):
                 # 64 entries long, 256 Bits = 64 Hex-digits
                 possible_secret = converter.float_array_to_hex(prediction[i], 64)
-                f.write(str(possible_secret))
-                f.write('\n')
+                f.write(f"{possible_secret}\n")
