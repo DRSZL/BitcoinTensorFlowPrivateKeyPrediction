2b2919ab2e3ec7a70350d8615a60c04390960ac8
David <162551705+DRSZL@users.noreply.github.com>
2026-02-22 23:02:02 +0100

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
+import argparse
 import numpy as np
 import trainingDataGenerator
 import converter
 import createModel
 import generateTxtFile
 
-# variables
-trainingDataLength = 1000
-seed = 1337
-file_name=f'prediction.keras'
-    
-# get training data
-y_bin_f32, xUncompressed_bin_f32, xCompressed_bin_f32, xSegwit_bin_f32 = trainingDataGenerator.createTrainingData(trainingDataLength, seed)
+def main():
+    parser = argparse.ArgumentParser(description='Bitcoin TensorFlow Private Key Prediction')
+    parser.add_argument('--seed', type=int, default=1337, help='Random seed for training data generation')
+    parser.add_argument('--length', type=int, default=1000, help='Number of training samples')
+    parser.add_argument('--model', type=str, default='prediction.keras', help='Model filename')
+    parser.add_argument('--create', action='store_true', help='Create and save a new model')
+    parser.add_argument('--predict', action='store_true', help='Load model and write predictions to txt')
+    parser.add_argument('--first-dim', type=int, default=512, help='First dense layer size')
+    parser.add_argument('--hidden-layers', type=int, default=8, help='Number of hidden layers')
+    parser.add_argument('--hidden-dim', type=int, default=256, help='Hidden layer size')
+    args = parser.parse_args()
 
-# flag to create and save a model
-create_Model = True
+    print(f"Generating {args.length} training samples with seed {args.seed}...")
+    y_bin_f32, xUncompressed_bin_f32, xCompressed_bin_f32, xSegwit_bin_f32 = trainingDataGenerator.createTrainingData(
+        args.length, args.seed
+    )
 
-if create_Model:
-    createModel.createAndSaveModel(16, 8, 8, file_name, xUncompressed_bin_f32, y_bin_f32)
+    if args.create:
+        print("Creating and saving model...")
+        createModel.createAndSaveModel(
+            args.first_dim, args.hidden_layers, args.hidden_dim,
+            args.model, xUncompressed_bin_f32, y_bin_f32
+        )
 
-# flag to load and write a txt from predictions
-loadModelAndWrite = False
-    
-if loadModelAndWrite: 
-    generateTxtFile.createTextFileFromModelPrediction(file_name, xUncompressed_bin_f32)
+    if args.predict:
+        print("Loading model and writing predictions...")
+        generateTxtFile.createTextFileFromModelPrediction(args.model, xUncompressed_bin_f32)
+
+
+if __name__ == '__main__':
+    main()
